24fc92eeaa1e4674dabf35494feffb858171afde
ArliArliwanh <arliarliwanh13@gmail.com>
2021-11-19 13:49:55 +0700

    commit

--- server.js
+++ server.js
@@ -70,7 +70,6 @@ const option = {
 const swaggerDocs = swaggerJSDoc(option)
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
-console.log("Hello world")
 
 //Call server
 app.listen(PORT, console.log(`Server Running on port ${PORT}`))
